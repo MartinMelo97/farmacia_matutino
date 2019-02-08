2f88dc93411af473b077d27192a3784557906b27
Martin Melo <nitram-210397@hotmail.com>
2019-02-08 15:10:56 -0600

    Agregamos rutas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home'
 import Products from './components/Products'
 import Info from './components/Info'
+import Patients from './components/Patients'
+import { Switch, Route, NavLink } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="main-container">
         <ul className="navbar">
-          <li>Home</li>
-          <li>Products</li>
-          <li>Info</li>
+         <NavLink to="/" style={{color: "#fff", textDecoration: "none"}}>
+            <li className="linksito">Home</li>
+          </NavLink>
+          <NavLink to="/products" style={{color: "#fff", textDecoration: "none"}}>
+            <li className="linksito">Products</li>
+          </NavLink>
+          <NavLink to="/info" style={{color: "#fff", textDecoration: "none"}}>
+            <li className="linksito">Info</li>
+          </NavLink>
+          <NavLink to="/patients" style={{color: "#fff", textDecoration: "none"}}>
+            <li className="linksito">Patients</li>
+          </NavLink>
         </ul>
-        <h1>Farmacias React</h1>
-        <div className="components">
-          <Home />
-          <Products />
-          <Info />
-        </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/info" component={Info} />
+          <Route exact path="/patients" component={Patients} />
+        </Switch>
       </div>
     );
   }
---
+++ src/components/Patients.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Patients = () => (
+    <div>
+    <h1>Nuestros pacientes quedan satisfechos</h1>
+    <img 
+    src="https://previews.123rf.com/images/hedgehog/hedgehog1310/hedgehog131000009/23555584-enfermera-de-gr%C3%A1fico-holding-y-pacientes-felices.jpg"
+    alt="Pacientes felices" />
+    </div>
+)
+
+export default Patients
